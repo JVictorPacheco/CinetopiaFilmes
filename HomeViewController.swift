41ea9158bed553095ae8d634581b6dd2cbaf0105
João Pacheco <joaovictormacielpacheco@gmail.com>
2025-02-19 22:48:25 -0300

    Criando a primeira label programaticamente
    Criando a primeira label programaticamente na primeira tela do app. Estamos iniciando a primeira tela e todo o seu layout

--- HomeViewController.swift
+++ HomeViewController.swift
@@ -6,15 +6,45 @@
 //
 
 import UIKit
+import SnapKit
 
 class HomeViewController: UIViewController {
 
+    //MARK: PROPERTIES
+    
+    private lazy var label: UILabel = {
+        let label = UILabel()
+        label.text = "Cinetopia Filmes"
+        label.textColor = .white
+        label.font = .systemFont(ofSize: 24, weight: .bold)
+        return label
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = .blue
+        view.backgroundColor = .background
+        setupView()
+        setupConstraints()
         // Do any additional setup after loading the view.
     }
-
-
+    
+    private func setupView() {
+        view.addSubview(label)
+    }
+    
+    private func setupConstraints() {
+        label.snp.makeConstraints{ make in
+            make.centerX.equalToSuperview()
+            make.centerY.equalToSuperview()
+            //make.trailing.equalToSuperview().inset(24)
+        }
+        
+        
+        // NSLayoutConstraint.active ([
+            //label.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            //label.centerYAnchor.constraint(equalTo: view.centerYAnchor)
+        //])
+        //
+    }
 }
 
